6232d8926d15cff8afcac2345fde6ae409406b6c
unknown <956608008@qq.com>
2016-01-26 14:43:51 +0800

    head

---
+++ head.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : head.cpp
+// Author      : lin
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 0;
+	int i = 0;
+	for(i = 0;i<3;i++){
+		cout<<a<<endl;
+		a++;
+	}
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "nihao" <<endl;
+	return 0;
+}
